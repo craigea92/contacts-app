21ddb1cc6e612401b692a4b01154c15d64ac96d0
Craig Eaton <craigea92@gmail.com>
2023-02-17 15:30:35 +0000

    Tested creating multiple card instances with different information from contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,18 @@ function App() {
         tel={contacts[0].phone}
         email={contacts[0].email}
       />
+      <Card 
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card 
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
